472841896c3ad658d94c028168670674f579f426
Marlloneves <marlloneves27@gmail.com>
2024-07-05 16:24:43 -0300

    Adding dependency injection to domain and app service

--- UneContChallenge.CrossCutting/DependencyInjector.cs
+++ UneContChallenge.CrossCutting/DependencyInjector.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using UneContChallenge.Application.Interfaces;
+using UneContChallenge.Application.Services;
 using UneContChallenge.Domain.Interfaces.Repositories;
+using UneContChallenge.Domain.Interfaces.Services;
+using UneContChallenge.Domain.Services;
 using UneContChallenge.Infra.Repositories;
 
 namespace UneContChallenge.CrossCutting
@@ -8,6 +12,12 @@ namespace UneContChallenge.CrossCutting
     {
         public static void Register(IServiceCollection services)
         {
+            //AppService
+            services.AddTransient<INotaFiscalAppService, NotaFiscalAppService>();
+
+            //Domain
+            services.AddTransient(typeof(IBaseDomainService<>), typeof(BaseDomainService<>));
+
             // Repository
             services.AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>));
             services.AddTransient<INotaFiscalRepository, NotaFiscalRepository>();
