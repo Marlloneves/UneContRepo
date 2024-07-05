118d0d6d473ba86247f123da0b9b6d269c68749f
Marlloneves <marlloneves27@gmail.com>
2024-07-05 15:58:35 -0300

    Feat: Initial Migration using EF

---
+++ UneContChallenge.Infra.SqlServer/Contexts/DataContextMigration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace UneContChallenge.Infra.Contexts
+{
+    public class DataContextMigration : IDesignTimeDbContextFactory<DataContexts>
+    {
+        public DataContexts CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.infra.json");
+            config.AddJsonFile(path, false);
+
+            var root = config.Build();
+            var connectionString = root.GetSection("ConnectionStrings").GetSection("UneContSettings").Value;
+
+            var builder = new DbContextOptionsBuilder<DataContexts>();
+            builder.UseSqlServer(connectionString);
+
+            return new DataContexts(builder.Options);
+        }
+    }
+}
---
+++ UneContChallenge.Infra.SqlServer/Migrations/20240705183750_Initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UneContChallenge.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotaFiscal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomePagador = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    NumeroIdentificacao = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    DataEmissao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataCobranca = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DataPagamento = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DocumentoNotaFiscal = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    DocumentoBoletoBancario = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotaFiscal", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotaFiscal");
+        }
+    }
+}
